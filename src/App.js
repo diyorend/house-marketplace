4841adfc7982954c1f8cbc9c90a8c9221ff9f7bb
Diyor End <diyorend@gmail.com>
2022-06-29 09:13:37 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//components
+import Navbar from "./components/Navbar";
+//Pages
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
-    <div className="App">
-      <h1>APP</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </BrowserRouter>
+    </>
   );
 }
 
